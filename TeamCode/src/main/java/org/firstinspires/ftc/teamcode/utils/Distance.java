d441d3f9c3ddfa0c6c6c922870e238c06c7a2af4
Ari Kamat <58306362+arikamat@users.noreply.github.com>
2020-08-09 22:05:23 -0400

    Update Distance.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Distance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Distance.java
@@ -1,1 +1,26 @@
+package org.firstinspires.ftc.teamcode.utils;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 
+public class Distance{
+	
+	DistanceSensor distanceSensor;
+	
+	public Color (HardwareMap hmap, String name){
+		this.distanceSensor = hardwareMap.get(DistanceSensor.class, name);
+  }
+  
+  public double getDistance(){
+		return getDistanceIn();
+	}
+
+	public double getDistanceIn(){
+		return (double) this.distanceSensor.getDistance(DistanceUnit.INCH);
+	}
+	
+	public double getDistanceCm(){
+		return (double) this.distanceSensor.getDistance(DistanceUnit.CM);
+	}
+
+}
